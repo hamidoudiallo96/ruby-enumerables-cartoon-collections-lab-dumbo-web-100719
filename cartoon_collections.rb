6d207aa68940f89dcedca6e0c127fa466a1a417a
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-27 19:43:44 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,9 +5,13 @@ def roll_call_dwarves(array)
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)
   # Your code here
-  
+  result_array = []
+  array.each do |word|
+    result_array << "#{word.capitalize}!"
+  end
+  return result_array
 end
 
 def long_planeteer_calls# code an argument here
