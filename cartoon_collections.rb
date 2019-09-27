c92ba6e010f1be624f3975ec92b74708c1fbb814
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-27 20:11:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,8 @@ def long_planeteer_calls(array)
    return status
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
