583d94174fb43a805e77238f57f2ce29ab842634
Hamidou Diallo <dio.hamidou@gmail.com>
2019-09-27 19:53:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  return array.all? {|word| word >= 4}
+  return array.all? {|word| word>4}
 end
 
 def find_the_cheese# code an argument here
